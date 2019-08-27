e38868ad1dbf3c1d2b5e45e94f5a26082ebe83c0
Nam-SH <gtsmell@gmail.com>
2019-08-27 20:06:28 +0900

    20190827 | 05. Queue completed

--- testy.py
+++ testy.py
@@ -1,35 +1,35 @@
 import sys; sys.stdin = open('data/(5102)input.txt', 'r')
 from collections import deque
 
-def find(arr, s, g):
+def bfs(s):
     global res
-    check = [False for _ in range(v + 1)]
-    q = deque()
     q.append(s)
-    check[s] = True
+    visit[s] = True
 
     while q:
         s = q.popleft()
         for k in arr[s]:
-            if not check[k] and arr[k]:
-                check[k] = True
+            if arr[k] and not visit[k]:
                 q.append(k)
-            if k == g:
-                return
+                visit[k] = True
+                num[k] = num[s] +1
+                if k == g:
+                    res = num[k]
+                    return
 
-T = int(input())
-for tc in range(T):
+for tc in range(int(input())):
     v, e = map(int, input().split())
-
-    arr = [[] for _ in range(v + 1)]
+    arr = [[0] * (v+1) for _ in range(v+1)]
+    visit = [False] * (v+1)
+    num = [0] * (v+1)
     res = 0
-
-
-    for _ in range(e):
+    for i in range(e):
         i, j = map(int, input().split())
         arr[i].append(j)
         arr[j].append(i)
+
     s, g = map(int, input().split())
-    find(arr, s, g)
-    print()
-    print()
+    q = deque()
+    bfs(s)
+
+    print('#{} {}'.format(tc + 1, res))
