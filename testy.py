4efead5ec6ce788501eccacdf130df54e547abab
Nam-SH <gtsmell@gmail.com>
2019-08-29 19:29:18 +0900

    20190829 | fix some code in 07. Linked_list_Club

--- testy.py
+++ testy.py
@@ -1,21 +1,35 @@
-T = int(input())
-​
-for tc in range(1, T+1):
-    N, M, length = map(int, input().split())
-    rc = [list(map(int, input().split())) for _ in range(N)]
+import sys; sys.stdin = open('data/(1230)input.txt', 'r')
 
-    for r in range(N - length + 1): # 행 N
-        for c in range(M - length + 1): # 열 M
-            S = S2 = 0 # 새로운 합을 계속 비교해야하니 0을 계속 할당해준다.
-            for i in range(r, r+length):
-                for j in range(c, c+length):
-                    S += rc[i][j]
-            # 작은 사각형도 같은 for문에 돌려주자
-            for ii in range(r+1, r+length -1): # 길이는 총 2인덱스가 빠져야 한다.
-            # 그래서 양옆을 각각 1더하고 1을 빼면 양옆이 2가 줄어든다.
-                for jj in range(c+1, c+length -1):
-                    S2 += rc[ii][jj]
-            result = S - S2
-            if result > Max:
-                Max = result
-    print('#{} {}'.format(tc, Max))
+for tc in range(10):
+    basic_n = int(input())
+    basic_data = input().split()
+
+    N = int(input())
+    data = input().split()
+
+    cnt = i = 0
+    while cnt != N:
+        if data[i] == 'I':
+            x = int(data[i + 1])
+            y = int(data[i + 2])
+            for j in range(i + 3, i + 3 + y):
+                basic_data.insert(x, data[j])
+                x += 1
+            i += 3 + y
+
+        elif data[i] == 'D':
+            x = int(data[i + 1])
+            y = int(data[i + 2])
+            for _ in range(y):
+                basic_data.pop(x)
+            i += 3
+
+        elif data[i] == 'A':
+            y = int(data[i + 1])
+            for j in range(i + 2, i + 2 + y):
+                basic_data.append(data[j])
+            i += 2
+
+        cnt += 1
+
+    print('#{}'.format(tc + 1), *basic_data[:10])
