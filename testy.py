62aef887311eff953c8f6d2dcf192a28dfeafc8d
Nam-SH <gtsmell@gmail.com>
2019-08-30 00:02:34 +0900

    20190830 | 06. Queue_Club complted

--- testy.py
+++ testy.py
@@ -1,12 +1,65 @@
-import sys; sys.stdin = open('data/(5789)input.txt', 'r')
+import sys; sys.stdin = open('data/(1238)input.txt', 'r')
+from collections import deque
 
-for tc in range(int(input())):
+def find(s):
+    q = deque()
+    q.append(s)
+    V[s] = True
 
-    N, Q = map(int, input().split())
-    arr = [0] * (N + 1)
-    for i in range(1, Q + 1):
-        l, r = map(int, input().split())
-        for j in range(l, r + 1):
-            arr[j] = i
+    while q:
+        u = q.popleft()
+        for w in G[u]:
+            if not V[w]:
+                V[w] = True
+                q.append(w)
+                D[w] = D[u] + 1
+    return
 
-    print('#' + str(tc + 1), *arr[1:])
+for tc in range(10):
+    N, S = map(int, input().split())
+    arr = list(map(int, input().split()))
+    G = [[] for _ in range(101)]
+    D = [0] * 101
+
+    V = [False] * 101
+    for i in range(0, N, 2):
+        G[arr[i]].append(arr[i + 1])
+    find(S)
+
+    for i in range(100, -1, -1):
+        if D[i] == max(D):
+            print('#{} {}'.format(tc + 1, i))
+            break
+
+
+
+# def check(start):
+#     max_D = 0
+#     Q.append(start)
+#     visit[start] = True
+#     while Q:
+#         v = Q.pop(0)
+#         for w in G[v]:
+#             if visit[w]: continue
+#             visit[w] = True
+#             Q.append(w)
+#             D[w] = D[v] + 1
+#             max_D = max(D[w], max_D)
+#     return max_D
+#
+#
+# for t in range(1, 11):
+#     N, S = map(int, input().split())
+#     arr = list(map(int, input().split()))
+#     G = [[] for _ in range(101)]
+#     D = [0] * 101
+#     Q = []
+#     visit = [False] * 101
+#     for i in range(N // 2):
+#         G[arr[i * 2]].append(arr[i * 2 + 1])
+#     print(G)
+#     max_D = check(S)
+#     for i in range(100, 0, -1):
+#         if D[i] == max_D:
+#             print('#{} {}'.format(t, i))
+#             break
