dbb45e91335d8a8679805cb28cc0db907a5dcc06
Nam-SH <gtsmell@gmail.com>
2019-08-29 22:55:04 +0900

    20190829 | IM

--- testy.py
+++ testy.py
@@ -1,35 +1,12 @@
-import sys; sys.stdin = open('data/(1230)input.txt', 'r')
+import sys; sys.stdin = open('data/(5789)input.txt', 'r')
 
-for tc in range(10):
-    basic_n = int(input())
-    basic_data = input().split()
+for tc in range(int(input())):
 
-    N = int(input())
-    data = input().split()
+    N, Q = map(int, input().split())
+    arr = [0] * (N + 1)
+    for i in range(1, Q + 1):
+        l, r = map(int, input().split())
+        for j in range(l, r + 1):
+            arr[j] = i
 
-    cnt = i = 0
-    while cnt != N:
-        if data[i] == 'I':
-            x = int(data[i + 1])
-            y = int(data[i + 2])
-            for j in range(i + 3, i + 3 + y):
-                basic_data.insert(x, data[j])
-                x += 1
-            i += 3 + y
-
-        elif data[i] == 'D':
-            x = int(data[i + 1])
-            y = int(data[i + 2])
-            for _ in range(y):
-                basic_data.pop(x)
-            i += 3
-
-        elif data[i] == 'A':
-            y = int(data[i + 1])
-            for j in range(i + 2, i + 2 + y):
-                basic_data.append(data[j])
-            i += 2
-
-        cnt += 1
-
-    print('#{}'.format(tc + 1), *basic_data[:10])
+    print('#' + str(tc + 1), *arr[1:])
