22071b48a92869d092b2aca7af4e6d077ed42b4e
Nam-SH <gtsmell@gmail.com>
2019-09-01 00:01:27 +0900

    20190901 | Rename and update

--- testy.py
+++ testy.py
@@ -1,24 +1,17 @@
 import sys; sys.stdin = open('data/(5110)input.txt', 'r')
-from collections import deque
 
-for tc in range(int(input())):
-    N, M = map(int, input().split())
-    q = deque()
-    q.extend(map(int, input().split()))
+from another_linked_list import LinkedList
 
-    for i in range(M - 1):
-        data = deque()
-        data.extend(map(int, input().split()))
-        if max(q) < data[0]:
-            q.extend(data)
-        else:
-            for j in range(len(q)):
-                if q[j] > data[0]:
-                    for k in range(len(data)):
-                        q.insert(j, data[k])
-                        j += 1
-                    break
 
-    print('#' + str(tc + 1), end = ' ')
-    for i in range(1, 11): print(q[-i], end=' ')
-    print()
+
+T = int(input())
+N, M = map(int, input().split())
+
+dl =LinkedList((list(map(int, input().split()))))
+
+for i in range(M - 1):
+    data = list(map(int, input().split()))
+    for j in list(dl):
+        if j > data[0]:
+
+
