93e2b11cbd5e84afabe6b893befdb5ba174e4406
Nam-SH <gtsmell@gmail.com>
2019-09-01 21:23:30 +0900

    20190901 | Add Code

--- testy.py
+++ testy.py
@@ -1,17 +1,21 @@
-import sys; sys.stdin = open('data/(5110)input.txt', 'r')
+import sys; sys.stdin = open('data/(5178)input.txt', 'r')
 
-from another_linked_list import LinkedList
+def find(n):
 
-
-
-T = int(input())
-N, M = map(int, input().split())
-
-dl =LinkedList((list(map(int, input().split()))))
-
-for i in range(M - 1):
-    data = list(map(int, input().split()))
-    for j in list(dl):
-        if j > data[0]:
+    l_n = n * 2
+    r_n = n * 2 + 1
+    if r_n > N and l_n > N: return
+    find(l_n)
+    find(r_n)
+    arr[n] = arr[r_n] + arr[l_n]
 
 
+for tc in range(int(input())):
+    N, M, L = map(int, input().split())
+    arr = [0]*(N + 1) + [0]
+
+    for _ in range(M):
+        idx, val = map(int, input().split())
+        arr[idx] = val
+    find(1)
+    print('#{} {}'.format(tc + 1, arr[L]))
