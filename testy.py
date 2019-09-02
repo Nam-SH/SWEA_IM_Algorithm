ac25d8426d0d52362fb91f097659cb9c713dff94
Nam-SH <gtsmell@gmail.com>
2019-09-02 16:00:54 +0900

    20190902 | Update code

--- testy.py
+++ testy.py
@@ -1,56 +1,15 @@
-import sys; sys.stdin = open('data/(5178)input.txt', 'r')
-
-<<<<<<< HEAD
-def find(n):
-
-    l_n = n * 2
-    r_n = n * 2 + 1
-    if r_n > N and l_n > N: return
-    find(l_n)
-    find(r_n)
-    arr[n] = arr[r_n] + arr[l_n]
-
+import sys; sys.stdin = open('data/(6485)input.txt', 'r')
 
 for tc in range(int(input())):
-    N, M, L = map(int, input().split())
-    arr = [0]*(N + 1) + [0]
+    N = int(input())
+    arr = [0] * 5001
 
-    for _ in range(M):
-        idx, val = map(int, input().split())
-        arr[idx] = val
-    find(1)
-    print('#{} {}'.format(tc + 1, arr[L]))
-=======
-def bfs(s):
-    global res
-    q.append(s)
-    visit[s] = True
+    for _ in range(N):
+        a, b = map(int, input().split())
+        for i in range(a, b + 1):
+            arr[i] += 1
 
-    while q:
-        s = q.popleft()
-        for k in arr[s]:
-            if arr[k] and not visit[k]:
-                q.append(k)
-                visit[k] = True
-                num[k] = num[s] +1
-                if k == g:
-                    res = num[k]
-                    return
-
-for tc in range(int(input())):
-    v, e = map(int, input().split())
-    arr = [[0] * (v+1) for _ in range(v+1)]
-    visit = [False] * (v+1)
-    num = [0] * (v+1)
-    res = 0
-    for i in range(e):
-        i, j = map(int, input().split())
-        arr[i].append(j)
-        arr[j].append(i)
-
-    s, g = map(int, input().split())
-    q = deque()
-    bfs(s)
-
-    print('#{} {}'.format(tc + 1, res))
->>>>>>> 74889f2... 20190827 |
+    print('#' + str(tc + 1), end=' ')
+    for _ in range(int(input())):
+        print(arr[int(input())], end = ' ')
+    print()
