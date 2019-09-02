bef1d0da8c272518d810e5e2fc5e6e132589742e
Nam-SH <gtsmell@gmail.com>
2019-09-02 23:56:20 +0900

    20190902 | Update code

--- testy.py
+++ testy.py
@@ -1,15 +1,29 @@
-import sys; sys.stdin = open('data/(6485)input.txt', 'r')
+import sys; sys.stdin = open('data/(1258)input.txt', 'r')
 
-for tc in range(int(input())):
-    N = int(input())
-    arr = [0] * 5001
 
-    for _ in range(N):
-        a, b = map(int, input().split())
-        for i in range(a, b + 1):
-            arr[i] += 1
+t = int(input())
+for tc in range(1):
+    n = int(input())
+    m = [[0] * n for _ in range(n)]
 
-    print('#' + str(tc + 1), end=' ')
-    for _ in range(int(input())):
-        print(arr[int(input())], end = ' ')
+    dic = dict()
+    for i in range(n):
+        num = 0
+        line = list(map(int, input().split()))
+        for j in range(n):
+            if line[j]: num += 1
+            elif num:
+                if num not in dic:
+                    dic[num] = 1
+                else:
+                    dic[num] += 1
+                num = 0
+    arr = []
+    for key in dic:
+        arr.append([dic[key], key, dic[key] * key])
+
+    arr.sort(key=lambda x: (x[2], x[0]))
+
+    print("#{} {}".format(tc + 1, len(arr)), end=' ')
+    for i in arr: print(i[0], i[1], end = ' ')
     print()
