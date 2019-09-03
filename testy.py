73b8b8a7f0e5e8139dccaeea2044eaced61c1ec5
Nam-SH <gtsmell@gmail.com>
2019-09-03 17:32:24 +0900

    20190903 | 07. Linked_List completed

--- testy.py
+++ testy.py
@@ -1,29 +1,15 @@
-import sys; sys.stdin = open('data/(1258)input.txt', 'r')
+import sys; sys.stdin = open('data/(5177)input.txt', 'r')
+from heapq import heapify
 
 
-t = int(input())
-for tc in range(1):
-    n = int(input())
-    m = [[0] * n for _ in range(n)]
-
-    dic = dict()
-    for i in range(n):
-        num = 0
-        line = list(map(int, input().split()))
-        for j in range(n):
-            if line[j]: num += 1
-            elif num:
-                if num not in dic:
-                    dic[num] = 1
-                else:
-                    dic[num] += 1
-                num = 0
-    arr = []
-    for key in dic:
-        arr.append([dic[key], key, dic[key] * key])
-
-    arr.sort(key=lambda x: (x[2], x[0]))
-
-    print("#{} {}".format(tc + 1, len(arr)), end=' ')
-    for i in arr: print(i[0], i[1], end = ' ')
-    print()
+for tc in range(int(input())):
+    N = int(input())
+    heap = list(map(int, input().split()))
+    heapify(heap)
+    print(heap)
+    heap = [0] + heap
+    res = 0
+    while N >= 1:
+        N //= 2
+        res += heap[N]
+    print('#{} {}'.format(tc + 1, res))
