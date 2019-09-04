959dcfa35d25b5e3c1584a3dad190a8ce8a327c1
Nam-SH <gtsmell@gmail.com>
2019-09-04 23:18:02 +0900

    20190904 | Update code

--- testy.py
+++ testy.py
@@ -1,15 +1,17 @@
-import sys; sys.stdin = open('data/(5177)input.txt', 'r')
-from heapq import heapify
-
+import sys; sys.stdin = open('data/(6190)input.txt', 'r')
 
 for tc in range(int(input())):
     N = int(input())
-    heap = list(map(int, input().split()))
-    heapify(heap)
-    print(heap)
-    heap = [0] + heap
-    res = 0
-    while N >= 1:
-        N //= 2
-        res += heap[N]
-    print('#{} {}'.format(tc + 1, res))
+    arr = list(map(int, input().split()))
+    Max = 0
+    for i in range(N - 1):
+        for j in range(i + 1, N):
+            val = arr[i] * arr[j]
+            if val > 11:
+                val = arr[i] * arr[j]
+                if val > 11 and len(set(str(val))) >= 2:
+                    if list(str(val)) == sorted(list(str(val))):
+                        if Max < val:
+                            Max = val
+    if Max == 0: print('#{} -1'.format(tc + 1))
+    else: print('#{} {}'.format(tc + 1, Max))
