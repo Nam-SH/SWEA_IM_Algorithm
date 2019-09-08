bf41720dd3884891e455c07e0e3297e654f90aac
Nam-SH <gtsmell@gmail.com>
2019-09-08 20:09:54 +0900

    20190908 | Rename

--- testy.py
+++ testy.py
@@ -1,65 +1,24 @@
-import sys; sys.stdin = open('data/(5110)input.txt', 'r')
+import sys; sys.stdin = open('data/(1227)input.txt', 'r')
+from collections import deque
 
-class Node:
-    def __init__(self, data, pre, link):
-        self.data = data
-        self.pre = pre
-        self.link = link
+def bfs(i, j):
+    q = deque()
+    q.append((i, j))
+    visit[i][j] = 1
+    while q:
+        i, j = q.popleft()
+        for dy, dx in (0, 1), (0, -1), (1, 0), (-1, 0):
+            ty = i + dy
+            tx = j + dx
+            if arr[ty][tx] == '3':
+                return 1
+            elif arr[ty][tx] == '0' and (ty, tx) and not visit[ty][tx]:
+                visit[ty][tx] = 1
+                q.append((ty, tx))
+    return 0
 
-def addLast(data):
-    global pHead
-    global pTail
-    if pHead == None:
-        pHead = Node(data, None, None)
-        pTail = pHead
-    else:
-        p = pHead
-        while p.link != None:
-            p = p.link
-        p.link = Node(data, p, None)
-        pTail = p.link
-    return
-
-
-def addNumbers():
-    global pHead
-    global pTail
-    p = pHead
-    if p == None:
-        for i in range(N): addLast(s[i])
-    else:
-        while p.link != None and p.data <= s[0]:
-            p = p.link
-        if p.data > s[0]:
-            if p.pre == None:
-                p.pre = Node(s[0], None, p)
-                pHead = p.pre
-                for i in range(1, N):
-                    p.pre.link = Node(s[i], p.pre, p)
-                    p.pre = p.pre.link
-            else:
-                for i in range(N):
-                    p.pre.link = Node(s[i], p.pre, p)
-                    p.pre = p.pre.link
-        else:
-            for i in range(N): addLast(s[i])
-    return
-
-
-def print_res():
-    global pTail
-    p = pTail
-    for i in range(10):
-        print(p.data, end=' ')
-        p = p.pre
-    print()
-
-for tc in range(int(input())):
-    N, M = map(int, input().split())
-    pHead = None
-    pTail = None
-    for i in range(M):
-        s = list(map(int, input().split()))
-        addNumbers()
-    print('#' + str(tc + 1), end=' ')
-    print_res()
+for _ in range(10):
+    tc = int(input())
+    arr = [input() for _ in range(100)]
+    visit = [[0] * 100 for _ in range(100)]
+    print('#{} {}'.format(tc, bfs(2, 1)))
