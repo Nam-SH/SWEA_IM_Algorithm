1b75421d202842882637b5c9d7de443fe7b9579d
Nam-SH <gtsmell@gmail.com>
2019-09-08 21:00:05 +0900

    20190908 | Update

--- testy.py
+++ testy.py
@@ -1,24 +1,19 @@
-import sys; sys.stdin = open('data/(1227)input.txt', 'r')
-from collections import deque
-
-def bfs(i, j):
-    q = deque()
-    q.append((i, j))
-    visit[i][j] = 1
-    while q:
-        i, j = q.popleft()
-        for dy, dx in (0, 1), (0, -1), (1, 0), (-1, 0):
-            ty = i + dy
-            tx = j + dx
-            if arr[ty][tx] == '3':
-                return 1
-            elif arr[ty][tx] == '0' and (ty, tx) and not visit[ty][tx]:
-                visit[ty][tx] = 1
-                q.append((ty, tx))
-    return 0
+import sys; sys.stdin = open('data/(1210)input.txt', 'r')
 
 for _ in range(10):
     tc = int(input())
-    arr = [input() for _ in range(100)]
-    visit = [[0] * 100 for _ in range(100)]
-    print('#{} {}'.format(tc, bfs(2, 1)))
+    arr = [input().split() for _ in range(100)]
+    y, x = 99, arr[99].index('2')
+    d = [-1, 1]
+
+    while y != 0:
+        y -= 1
+        for i in range(2):
+            tx = x + d[i]
+            if 0 <= tx < 100 and arr[y][tx] == '1':
+                while 0 <= tx < 100 and arr[y][tx] == '1':
+                    tx += d[i]
+                x = tx
+                break
+
+    print('#%d %d' % (tc, x))
